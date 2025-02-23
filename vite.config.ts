5e4ae165eea1cc336e130388692620a01d16c85c
Hamza Shakeel <hamzashakeel348@gmail.com>
2025-02-24 01:35:36 +0500

    build: configure Vite and TypeScript setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Ensures the app is accessible outside the container
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // Ensures live reload works in Docker
+    },
+  },
 })
